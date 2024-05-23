07a1ebff3a894a1e12b454c56f954ce94555a711
LingannaJ <jlinganna104@gmail.com>
2024-05-23 14:36:38 +0530

    terraform-multi

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,5 @@
+    
+    bucket = "multi-env-devops"
+    key    = "multi-env-practice"
+    region = "us-east-1"
+    dynamodb_table = "multi-env-dev"
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,5 @@
+    
+    bucket = "multi-env-prodc"
+    key    = "multi-env-practice"
+    region = "us-east-1"
+    dynamodb_table = "multi-env-prod"
